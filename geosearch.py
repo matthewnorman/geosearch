ffd0499783082c04aba37a005174c2b13524333b
Matthew Norman <matthewn@surveymonkey.com>
2016-01-12 20:16:14 -0800

    First working version of a geosearcher

--- geosearch.py
+++ geosearch.py
@@ -1,5 +1,6 @@
 import pytrie
 import geohash
+from haversine import haversine
 
 
 class LatLonGeolocator():
@@ -10,6 +11,7 @@ class LatLonGeolocator():
 
     def __init__(self):
         self.storage = pytrie.StringTrie()
+        self.points_by_id = {}
 
     def _encode_and_store_(self, latitude, longitude, ID):
         """
@@ -21,6 +23,7 @@ class LatLonGeolocator():
         """
         hash = geohash.encode(latitude=latitude, longitude=longitude)
         self.storage[hash] = ID
+        self.points_by_id[ID] = (latitude, longitude)
 
     def _decode_by_hash(self, hash):
         """
@@ -31,3 +34,87 @@ class LatLonGeolocator():
 
         return self.storage[hash]
 
+    def _get_adjoining_hashes(self, hashcode, precision):
+        """
+        Given a hash, find the nearest eight boxes (four adjacent,
+        four diagonal) to the box.
+
+        :param hashcode: Initial full geohash
+        :param precision: Level of precision, integer
+        :return:
+        """
+        if precision > len(hashcode):
+            raise ValueError('Precision greater than hashcode size.')
+        reduced_hash = hashcode[:precision]
+
+        return geohash.neighbors(reduced_hash)
+
+    def add_location(self, latitude, longitude, ID):
+        """
+        Accessor to add IDs
+        :param latitude: Floating point
+        :param longitude: Floating point
+        :param id: Global ID
+        :return:
+        """
+
+        self._encode_and_store_(latitude=latitude, longitude=longitude,
+                                ID=ID)
+
+    def proximity_search(self, latitude, longitude, radius):
+        """
+        Given a centerpoint, find everything within a radius around
+        that latitude and longitude, returned in order.
+
+        :param latitude: floating point latitude
+        :param longitude: floating point longitude
+        :param radius: radius in meters.
+        :return:
+        """
+
+        hashcode = geohash.encode(latitude=latitude, longitude=longitude)
+        centerpoint = (latitude, longitude)
+
+        tmp_hashcode = ''
+        for x in hashcode:
+            # Go through the hashcode character by character
+            tmp_hashcode += x
+            lat, lng, delta_lat, delta_lng = geohash.decode(tmp_hashcode,
+                                                            delta=True)
+            overall_lat = 2 * 1000 * haversine(
+                point1=(latitude - delta_lat, longitude),
+                point2=(latitude + delta_lat, longitude)
+            )
+            overall_lng = 2 * 1000 * haversine(
+                point1=(latitude, longitude-delta_lng),
+                point2=(latitude, longitude+delta_lng)
+            )
+
+            dist = min(overall_lng, overall_lat)
+            if dist < radius:
+                break
+
+        if tmp_hashcode == '':
+            raise ValueError('Radius larger than earth')
+
+        precision = len(tmp_hashcode)
+
+        search_hashes = self._get_adjoining_hashes(hashcode=hashcode,
+                                                   precision=precision)
+        search_hashes.append(tmp_hashcode)
+
+        possible_points = []
+        result_values = []
+
+        for search_hash in search_hashes:
+            possible_points.extend(self.storage.values(prefix=search_hash))
+
+        for point_id in possible_points:
+            point = self.points_by_id[point_id]
+            dist = haversine(centerpoint, point)
+            if dist <= radius:
+                result_values.append((point_id, dist))
+
+        sorted_results = sorted(result_values, key = lambda x: x[1])
+        final_results = [x[0] for x in sorted_results]
+        return final_results
--- tests/test_geosearch.py
+++ tests/test_geosearch.py
@@ -18,3 +18,49 @@ def test_encoding_decoding():
 
     result = geolocator._decode_by_hash(hash='gcpuvpjhmu89')
     assert result == ID
+
+
+def test_adjoining_boxes():
+    """
+    What are the nearest eight boxes to the Palace
+    :return:
+    """
+
+    geolocator = geosearch.LatLonGeolocator()
+    hashcode = 'gcpuvpjhmu89'
+
+    with pytest.raises(ValueError):
+        geolocator._get_adjoining_hashes(hashcode=hashcode,
+                                         precision=15)
+
+    result = geolocator._get_adjoining_hashes(hashcode=hashcode,
+                                              precision=12)
+
+    assert result == ['gcpuvpjhmu83', 'gcpuvpjhmu8c', 'gcpuvpjhmu88',
+                      'gcpuvpjhmu82', 'gcpuvpjhmu8b', 'gcpuvpjhmu8d',
+                      'gcpuvpjhmu86', 'gcpuvpjhmu8f']
+
+    result = geolocator._get_adjoining_hashes(hashcode=hashcode,
+                                              precision=6)
+    assert result == ['gcpuuz', 'gcpuvr', 'gcpuvn', 'gcpuuy',
+                      'gcpuvq', 'gcpvj0', 'gcpvhb', 'gcpvj2']
+
+
+def test_proximity_search():
+    """
+    Search for something around the Palace of Westminster.
+    :return:
+    """
+
+    geolocator = geosearch.LatLonGeolocator()
+    geolocator.add_location(latitude=51.499168, longitude=-0.124722, ID=2)
+    geolocator.add_location(latitude=51.499178, longitude=-0.124722, ID=3)
+    geolocator.add_location(latitude=51.499268, longitude=-0.124722, ID=4)
+    geolocator.add_location(latitude=51.500168, longitude=-0.124722, ID=5)
+    geolocator.add_location(latitude=51.699168, longitude=-0.124722, ID=6)
+    geolocator.add_location(latitude=52.499168, longitude=-0.124722, ID=7)
+    results = geolocator.proximity_search(latitude=51.499167,
+                                          longitude=-0.124722,
+                                          radius=1000)
+
+    assert results == [2, 3, 4, 5]
